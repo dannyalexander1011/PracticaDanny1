b3231311d182a8597a29d8fbd49e54fa3d4bc063
dannyalexander1011 <dannyalexander1011@gmail.com>
2017-05-05 07:50:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ingenieria/imc/Historico.java
@@ -0,0 +1,17 @@
+package com.example.ingenieria.imc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Historico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historico);
+    }
+    public void regresar(View v) {
+        finish();
+    }
+}
